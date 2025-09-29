9e3d3f24189483ec6a7de0f6a0c5084eb789afc5
Kelson Petersen <kelsonpetersen@icloud.com>
2025-09-29 17:11:13 -0600

    update main.c

--- prog_1/main.c
+++ prog_1/main.c
@@ -3,9 +3,9 @@
 #include <stdbool.h>
 
 // Functions for the problem
-void problem_1(void);
-void problem_2(void);
-void problem_3(void);
+int problem_1(void);
+int problem_2(void);
+int problem_3(void);
 
 // 3x3 matrix functions
 void mat_scale(double scale, double mat[][3], double prod[][3]);
@@ -20,21 +20,17 @@ int main(int argc, char** argv){
   } else {
     problem = atoi(argv[1]);            // Assign the value from the command line
   }
-  FILE *fout = fopen("dump_file","w");
-  if(fout == NULL){
-    perror("output file failed");
-    return EXIT_FAILURE;
-  }
+  
   bool done = false;
   char count = 0;
   while(!done){
     (count > 2) ? done = true: count++;
     switch(problem){
-      case 1: problem_1();
+      case 1: problem_1() ? printf("Problem 1 success\n"): printf("Error in Problem 1\n");
         break;
-      case 2: problem_2();
+      case 2: problem_2() ? printf("Problem 1 success\n"): printf("Error in Problem 1\n");
         break;
-      case 3: problem_3();
+      case 3: problem_3() ? printf("Problem 1 success\n"): printf("Error in Problem 1\n");
         break;
       default: 
         printf("Error please choose 1-3.\n");
@@ -45,14 +41,34 @@ int main(int argc, char** argv){
 }
 
 
-void problem_1(void){
+int problem_1(void){
+FILE *fout = fopen("prog_sol_1.txt","w");
+  if(fout == NULL){
+    perror("output file failed");
+    return EXIT_FAILURE;
+  }
 
-}
-void problem_2(void){
 
+  return EXIT_SUCCESS;
 }
-void problem_3(void){
-  
+// Problem 2 Code
+int problem_2(void){
+FILE *fout = fopen("dump_file","w");
+  if(fout == NULL){
+    perror("output file failed");
+    return EXIT_FAILURE;
+  }
+
+
+  return EXIT_SUCCESS;
+}
+// Problem 3 Code
+int problem_3(void){
+ FILE *fout = fopen("dump_file","w");
+  if(fout == NULL){
+    perror("output file failed");
+    return EXIT_FAILURE;
+  } 
 }
 
 // scale matrix `mat` by a and save in prod
