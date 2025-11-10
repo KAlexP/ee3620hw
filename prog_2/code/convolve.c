0a9782a68fadcf43b0cd429567399819002f0607
Kelson Petersen <kelsonpetersen@icloud.com>
2025-11-09 23:13:36 -0700

    hw10 setup + convolution code update

--- prog_2/code/convolve.c
+++ prog_2/code/convolve.c
@@ -1,19 +1,22 @@
 #include "convolve.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 int conv(double *f1, int len1, double *f2, int len2, double **y){
   int leny = len1 + len2 - 1;
   int k,m;
-  int in_b, out_b;
+  int in_b, m_start;
   double sum = 0;
   // Allocate The proper amount of memory for y
   *y = malloc((sizeof(double) * leny));
   for(k=0; k < leny; ++k){
-    out_b = (k - (len1-1)) > 0 ? (k - (len1 - 1)) : 0;
+    m_start = (k - (len1-1)) > 0 ? (k - (len1 - 1)) : 0;
     in_b = (k < len2) < len2   ? k : (len2 - 1);
-    sum = 0;
-    for(m=out_b; m < in_b && m < len1; ++m){
-      sum += f1[m] * f2[k-m];
+    sum = 0.0;
+    printf("\n\n");
+    for(m=m_start; m < in_b && m < len1; ++m){
+      sum += f1[m] * f2[k-m-1];
+      printf("f1[%d] * f2[%d] = %lf\tsum: %lf\n", m, k-m, f1[m] * f2[k-m],sum);
     }
     (*y)[k] = sum;
   }
--- prog_2/code/main.c
+++ prog_2/code/main.c
@@ -10,28 +10,43 @@ int main(void){
   const double f2[len2] = {-2,-2,-2,-2,-2,-2,-2};
   const double f3[len3] = {1,-1,1,-1};
   const double f4[len4] = {0,0,0,-3,-3};
+  const double X[3] =  {0.4,0.35,0.25};
+  const double Y[4] = {0.25,0.20,0.20,0.35};
+
  
   printf("f1: ");
   for(itr = 0; itr < len1; itr++){
-    printf("%5.0lf", f1[itr]);
+    printf(" %5lf", f1[itr]);
   }
   printf("\nf2: ");
   for(itr = 0; itr < len2; itr++){
-    printf("%5.0lf", f2[itr]);
+    printf(" %5lf", f2[itr]);
   }
   printf("\nf3: ");
   for(itr = 0; itr < len3; itr++){
-    printf("%5.0lf", f3[itr]);
+    printf(" %5lf", f3[itr]);
   }
   printf("\nf4: ");
   for(itr = 0; itr < len4; itr++){
-    printf("%5.0lf", f4[itr]);
+    printf(" %5lf", f4[itr]);
   }
   printf("\n\n");
   leny = conv(f1,len1,f1,len1,&y);
   printf("f1 * f1: ");
   for(itr = 0; itr < leny; ++itr){
-    printf("%5.0lf",y[itr]);
+    printf(" %5lf",y[itr]);
+  }
+  free(y);
+  printf("\nf1 * f2: ");
+  leny = conv(f1,len1,f2,len2, &y);
+  for(itr = 0; itr < leny; ++itr){
+    printf(" %5lf",y[itr]);
+  }
+  free(y);
+  printf("\nX * Y: ");
+  leny = conv(X,3,Y,4, &y);
+  for(itr = 0; itr < leny; ++itr){
+    printf(" %5lf",y[itr]);
   }
   free(y);
   return 0;
