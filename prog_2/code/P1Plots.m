c845c1ec0bdf5412f65530c960351a485e34bd6e
Kelson Petersen <kelsonpetersen@icloud.com>
2025-11-14 21:33:24 -0700

    adds fig files

--- prog_2/code/P1Plots.m
+++ prog_2/code/P1Plots.m
@@ -3,7 +3,31 @@ f2 = [-2,-2,-2,-2,-2,-2,-2];
 f3 = [1,-1,1,-1];
 f4 = [0,0,0,-3,-3];
 
-plot(0:1:5,f1)
-plot(0:1:6,f2)
-plot(0:1:3,f3)
-plot(0:1:4,f4)
+% Compute convolutions
+c1 = conv(f1, f1);
+c2 = conv(f1, f2);
+c3 = conv(f1, f3);
+c4 = conv(f2, f3);
+c5 = conv(f1, f4);
+
+figure;
+
+subplot(5,1,1);
+stem(0:length(c1)-1, c1, 'filled');
+title('f1 * f1');
+
+subplot(5,1,2);
+stem(0:length(c2)-1, c2, 'filled');
+title('f1 * f2');
+
+subplot(5,1,3);
+stem(0:length(c3)-1, c3, 'filled');
+title('f1 * f3');
+
+subplot(5,1,4);
+stem(0:length(c4)-1, c4, 'filled');
+title('f2 * f3');
+
+subplot(5,1,5);
+stem(0:length(c5)-1, c5, 'filled');
+title('f1 * f4');
--- prog_2/code/zero_state.c
+++ prog_2/code/zero_state.c
@@ -8,7 +8,7 @@ int main(void){
   double result = 0;
   FILE *fout = fopen("zero_state.txt","w");
   for(c_dt =0; c_dt < stop; c_dt += d_T){
-    result = -0.175 * exp(-2.604*c_dt) + 2 * (0.201*exp(-1.108*c_dt)*cos(-2.08*c_dt + 1.12));
+    result = -0.175 * exp(-2.604*c_dt) + 2 * (0.201*exp(-1.198*c_dt)*cos(2.08*c_dt + 1.12));
     fprintf(fout,"%lf\n",result);
   }
   fclose(fout);
